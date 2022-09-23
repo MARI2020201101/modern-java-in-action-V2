55aa8c7ce87bfda0994f6966b916d4691df79968
MARI <mari20201101@gmail.com>
2022-09-23 15:25:44 +0900

    Solve a problem of Rectangle to Sqare by another algorithm

---
+++ src/algorithm02/square/Square2.java
@@ -0,0 +1,32 @@
+package algorithm02.square;
+
+class Square2 {
+
+    public static void main(String[] args) {
+        System.out.println(check20Square());
+    }
+
+    static int check20Square(){
+        int count = 0;
+        for(int l = 1000; l > 0; l--){
+            for(int s = l; s > 0 ; s--){
+                if(countSquare(l,s)==20) count++;
+            }
+        }
+        return count;
+    }
+    static int countSquare(int l, int s){
+        if(l==s) return 1;
+        int result = 0;
+        if(l < s){
+            int tmp = s;
+            s = l;
+            l = tmp;
+        }
+        int share = l/s;
+        int remain = l%s;
+        result += share;
+        if(remain != 0) result += countSquare(s, remain);
+        return result;
+    }
+}
