9aa5a0df087775b7a26fa45f23fe5aa5d2bebec5
MARI <mari20201101@gmail.com>
2022-09-26 15:35:28 +0900

    Solve min cost route problem with Dynamic Programming

---
+++ src/dp05/DpMinCost.java
@@ -0,0 +1,33 @@
+package dp05;
+
+class DpMinCost {
+    static int[][] cost = {
+            {1,3,5,8},
+            {4,2,1,7},
+            {4,3,2,3}
+    };
+    static int[][] memo = new int[cost.length][cost[0].length];
+    public static void main(String[] args) {
+        System.out.println(solve(2,3));
+        print();
+    }
+    static void print(){
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                System.out.print(memo[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static int solve(int p, int q){
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                if(i==0 && j==0) memo[i][j]=cost[i][j];
+                else if(i==0) memo[i][j]+=cost[i][j-1] + cost[i][j];
+                else if(j==0) memo[i][j]+=cost[i-1][j] + cost[i][j];
+                else memo[i][j] = Math.min(memo[i][j-1], memo[i-1][j]) + cost[i][j];
+            }
+        }
+        return memo[p][q];
+    }
+}
